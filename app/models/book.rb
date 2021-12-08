00a905046631184245b6ca45f5acc1cc9493683c
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-08 23:06:05 +0900

    日本語および英語のロケール化の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@
 
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
+
+  # バリデーションのエラーメッセージを確認するために追加
+  validates :title, presence: true
+  validates :author, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # プリケーションでの利用が許されるロケールの許可リスト
+    config.available_locales = [:ja, :en]
+
+    # デフォルトロケールの設定
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
