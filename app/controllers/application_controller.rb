65a5d384d4bcb25a2de11cd41ebe86a7c0864cba
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-16 22:58:26 +0900

    ストロングパラメータを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation zip_code address profile])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation current_password zip_code address profile])
+  end
 end
