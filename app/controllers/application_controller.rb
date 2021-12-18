f82dee23068fef4c24c6dab0e8e14283d2fb5247
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-18 11:16:06 +0900

    ストロングパラメータの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation zip_code address profile])
-    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password remember_me])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[email password password_confirmation current_password zip_code address profile])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[zip_code address profile])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[password remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zip_code address profile])
   end
 end
