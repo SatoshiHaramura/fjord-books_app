4d939e6bad0665a454ab4559b3cc7638dc4bb3c5
SatoshiHaramura <satoshi.haramura@gmail.com>
2021-12-19 21:33:46 +0900

    アイコン画像の拡張子を制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :icon_image
+
+  validates :icon_image, attached: true, content_type: %i[jpg png gif]
 end
