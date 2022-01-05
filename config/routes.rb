5bcf44c7f9579d4f2761cfea409874bf88220d2d
SatoshiHaramura <satoshi.haramura@gmail.com>
2022-01-05 22:38:38 +0900

    commentsリソースフルルートの追加および共通化

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
-  resources :reports
+  concern :commentable do
+    resources :comments, only: %i[create destroy]
+  end
+  resources :books, concerns: :commentable
+  resources :reports, concerns: :commentable
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
